868d9c0e875584d32dad2ee4e3c3c124c7035035
EdwardMevis <edward.silva@fatec.sp.gov.br>
2025-12-02 09:40:00 -0300

    feat: adicionada classe principal Mod3LambdaApplication

---
+++ src/main/java/com/mod3/lambda/Mod3LambdaApplication.java
@@ -0,0 +1,12 @@
+package com.mod3.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod3LambdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mod3LambdaApplication.class, args);
+    }
+}
